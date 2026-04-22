8676f6b2a011217b121ca3dc76fa98fe1bd4d1d4
jasnoorranu619-dev <jasnoorranu619@gmail.com>
2026-04-22 10:32:02 -0700

    f
    f

--- main.py
+++ main.py
@@ -17,12 +17,23 @@ def calculate_bmi():
     lable_bmi['text'] = f"BMI: {bmi}" 
 #create and place the GUI widgets on the grid 
 #lable widgets 
-lable_mass = tk.Lable (root,text="mass
+lable_mass = tk.Lable (root, text="mass: ")
+lable_mass.grid(column=0, row=0)
 #entry widgets 
+enetry_mass = tk.Entry(root)
+enetry_mass.grid(column=1, row=0)
 #lable widgets 
+lable_height = tk.Lable(root, text="Height:")
+lable_height.grid(column=0, row=1)
 #entry widgets 
+enetry_height = tk.Enetry(root)
+enetry_height.grid(column=1,row=1 )
 #button widgets 
+button_calculate =tk.button(root, text="calculate", command =calculate_bmi)
+button_calculate.grid(column=0, row=2)
 #lable widgets 
+lable_bmi =tk.Lable(root, text="BMI:")
+lable_bmi.grid(column=1, row=2) 
 
 #Event loop and keep  the gui running 
 root.mainloop()
